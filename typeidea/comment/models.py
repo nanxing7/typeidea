a3353726865b76ce56355e09c46c5b827f1d075e
sanyuedev <sanyuedev@gmail.com>
2019-11-08 01:31:16 +0800

    初始化提交

---
+++ typeidea/comment/models.py
@@ -0,0 +1,12 @@
+
+from blog import Post
+# Create your models here.
+
+class Comment(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除'),
+    )
+    
