fadf94a0133791aefccef08f3b9886f5dd85e5dc
sanyuedev <sanyuedev@gmail.com>
2019-11-25 12:48:29 +0800

    缓存原理

--- typeidea/blog/apis.py
+++ typeidea/blog/apis.py
@@ -8,7 +8,7 @@
 from rest_framework.permissions import IsAdminUser
 
 from .models import Post, Category, Tag
-from .serializers import PostSerializer, PostDetailSerializer, CategorySerializer
+from .serializers import PostSerializer, PostDetailSerializer, CategorySerializer, CategoryDetailSerializer
 from rest_framework import viewsets
 
 
@@ -24,13 +24,21 @@ class PostViewSet(viewsets.ReadOnlyModelViewSet):
         return super().retrieve(request, *args, **kwargs)
 
     def filter_queryset(self, queryset):
-        # 获取参数 self.request.query_params.get('category')
+        # 获取参数 self.request.query_params.get('category') 即 category?category=id
+        # 不是 /category/id 之前一直搞错
         category_id = self.request.query_params.get('category')
         if category_id:
             queryset = queryset.filter(category_id=category_id)
+        print(category_id)
         return queryset
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.filter(status=Post.STATUS_NORMAL)
+
+    # TODO:学习这个函数
+    def retrieve(self, request, *args, **kwargs):
+        """重写retrieve 更改获取单个数据为多个数据"""
+        self.serializer_class = CategoryDetailSerializer
+        return super().retrieve(request, *args, **kwargs)
--- typeidea/blog/serializers.py
+++ typeidea/blog/serializers.py
@@ -6,12 +6,12 @@
 # @File    : serializers.py
 # @Software: PyCharm
 
-from rest_framework import serializers
+from rest_framework import serializers, pagination
 
 from .models import Post, Category
 
 
-class PostSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.HyperlinkedModelSerializer):
     """文章序列化器"""
     category = serializers.SlugRelatedField(
         read_only=True,
@@ -28,18 +28,43 @@ class PostSerializer(serializers.ModelSerializer):
     )
     created_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M-%S")
 
+    url = serializers.HyperlinkedIdentityField(view_name='api-post-detail')
+
     class Meta:
         model = Post
-        fields = ['id', 'title', 'category', 'tags', 'owner', 'created_time']
+        fields = ['url', 'id', 'title', 'category', 'tags', 'owner', 'created_time']
 
 
 class PostDetailSerializer(PostSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'title', 'category', 'tags', 'owner', 'content_html', 'created_time']
+        fields = ['url', 'id', 'title', 'category', 'tags', 'owner', 'content_html', 'created_time']
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name', 'created_time',)
+
+
+class CategoryDetailSerializer(CategorySerializer):
+    posts = serializers.SerializerMethodField('paginated_posts')
+
+    def paginated_posts(self, obj):
+        """实现对某个分类下文章列表的获取和分页"""
+        posts = obj.post_set.filter(status=Post.STATUS_NORMAL)
+        paginator = pagination.PageNumberPagination()
+        page = paginator.paginate_queryset(posts, self.context['request'])
+        serializer = PostSerializer(page, many=True, context={'request': self.context['request']})
+        return {
+            'count': posts.count(),
+            'results': serializer.data,
+            'previous': paginator.get_previous_link(),
+            'next': paginator.get_next_link(),
+        }
+
+    class Meta:
+        model = Category
+        fields = (
+            'id', 'name', 'created_time', 'posts'
+        )
